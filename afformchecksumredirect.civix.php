0f04235479d712b15c10f88589e7c2afe9e791b6
Matthew Wire (MJW) <mjw@mjwconsult.co.uk>
2026-02-03 13:33:49 +0000

    Upgrade civix

--- afformchecksumredirect.civix.php
+++ afformchecksumredirect.civix.php
@@ -75,7 +75,6 @@ class CRM_Afformchecksumredirect_ExtensionUtil {
     return self::CLASS_PREFIX . '_' . str_replace('\\', '_', $suffix);
   }
 
-
 }
 
 use CRM_Afformchecksumredirect_ExtensionUtil as E;
